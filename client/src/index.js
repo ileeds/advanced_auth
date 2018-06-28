1ee65cc633d85fdd3123a8bac6c151c5a5674342
ileeds <ileeds@brandeis.edu>
2018-06-27 20:46:09 -0400

    complete

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import { BrowserRouter, Route } from 'react-router-dom';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import reduxThunk from 'redux-thunk';
+import reducers from './reducers';
+import App from './components/App';
+import Welcome from './components/Welcome';
+import Signup from './components/auth/Signup';
+import Feature from './components/Feature';
+import Signout from './components/auth/Signout';
+import Signin from './components/auth/Signin';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+const store = createStore(reducers, {
+  auth: { authenticated: localStorage.getItem('token') }
+}, applyMiddleware(reduxThunk));
+
+ReactDOM.render(
+  <Provider store={store}>
+    <BrowserRouter>
+      <App>
+        <Route path='/' exact component={Welcome} />
+        <Route path='/signup' exact component={Signup} />
+        <Route path='/feature' exact component={Feature} />
+        <Route path='/signout' exact component={Signout} />
+        <Route path='/signin' exact component={Signin} />
+      </App>
+    </BrowserRouter>
+  </Provider>,
+  document.querySelector('#root')
+);
--- server/controllers/authentication.js
+++ server/controllers/authentication.js
@@ -15,13 +15,26 @@ exports.signup = function(req, res, next) {
   const email = req.body.email;
   const password = req.body.password;
 
-  const user = new User({
-    email: email,
-    password: password
-  });
+  if (!email || !password) {
+    return res.status(422).send({ error: 'You must provide email and password'});
+  }
 
-  user.save(function(err) {
+  User.findOne({ email: email }, function(err, existingUser) {
     if (err) { return next(err); }
-    res.json({ token: tokenForUser(user) });
+
+    if (existingUser) {
+      return res.status(422).send({ error: 'Email is in use' });
+    }
+
+    const user = new User({
+      email: email,
+      password: password
+    });
+
+    user.save(function(err) {
+      if (err) { return next(err); }
+
+      res.json({ token: tokenForUser(user) });
+    });
   });
 }
